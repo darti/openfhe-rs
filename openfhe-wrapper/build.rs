5ea6578d07e0b89b1198f32a171d72c55997cc5b
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2023-04-08 11:37:49 +0100

    Add bindgen to wrapper

--- openfhe-wrapper/build.rs
+++ openfhe-wrapper/build.rs
@@ -1,10 +1,8 @@
-use std::env;
+use std::{env, path::PathBuf};
 
 use cmake::Config;
 
-fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
+fn build_wrapper() -> PathBuf {
     let mut cfg = Config::new("wrapper");
 
     if let Some(include) = std::env::var_os("DEP_OPENFHE_INCLUDE") {
@@ -17,5 +15,30 @@ fn main() {
 
     println!("DEP_Z_INCLUDE: {:?}", std::env::var_os("DEP_Z_INCLUDE"));
 
-    cfg.build();
+    cfg.build()
+}
+
+fn generate_binding() {
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper/src/wrapper.hpp")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
+fn main() {
+    let _dst = build_wrapper();
+    generate_binding();
 }
